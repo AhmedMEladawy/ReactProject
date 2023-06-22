a6dc6c52d2bbef7cfecda4ddb5c6cbca31908837
AhmedMEladawy <81447515+AhmedMEladawy@users.noreply.github.com>
2023-06-22 13:52:47 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./NavBar.js";
+import Cards from './Cards.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Cards/>
+    </>
+  );
+}
+
+export default App;
